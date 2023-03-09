7223269b3eeffb7de692a025eea9042a67d6fce4
zinebaboumejd <zinebaboumejd@gmail.com>
2023-03-09 12:21:55 +0100

    activation clocalisation

--- MFN_frontend/pages/Maps.js
+++ MFN_frontend/pages/Maps.js
@@ -4,42 +4,67 @@ import { StyleSheet, Text, View ,Image} from 'react-native';
 import axios from "axios";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import MapView, { Callout, Circle, Marker } from 'react-native-maps';
-
+import * as Location from 'expo-location';
 
 export default function Maps(){
+    const [pin,setPin]=useState(
+        {
+            latitude: 32.3023,
+            longitude: -9.2411,
+        }
+    )
+    const [location, setLocation] = useState(null);
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      setPin({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      })
+        console.log("location",location)
+    })();
+  }, []);
     return (
         <View style={styles.container}>
             <MapView
                 style={styles.map}
-                initialRegion={{
-                    latitude: 32.3023,
-                    longitude: -9.2411,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
-                }}
+                initialRegion={location}
             >
                 <Marker
-                    coordinate={{
-                        latitude: 32.3023,
-                       longitude: -9.2411,
-                    }}
+                    coordinate={pin}
                     title="My Marker"
                     description="Some description"
                     pinColor='gold'
                     draggable={true}
-                    onDragEnd={(e) => console.log(e.nativeEvent.coordinate)}
+                    onDragEnd={(e) =>{
+                        console.log(e.nativeEvent.coordinate)
+                        setPin({
+                            latitude: e.nativeEvent.coordinate.latitude,
+                            longitude: e.nativeEvent.coordinate.longitude,
+                        })
+                        console.log("pin",pin)
+                    }
+                    }
                 >
                     <Callout>
                         <Text>this is a Callout</Text>
                     </Callout>
                     </Marker>
-                    <Circle
+                    {/* <Circle
                         center={{   
                             latitude: 32.3023,
                             longitude: -9.2411,
                         }}
                         radius={1000}
-                    />
+                    /> */}
             </MapView>
         </View>
     );
